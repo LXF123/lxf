49f013e12a852f14de966983938e63545a826566
luxuefang <1792754419@qq.com>
2015-11-09 10:07:13 +0800

    搭建框架

---
+++ 远程仓库/远程仓库/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  远程仓库
+//
+//  Created by lanou on 15/11/9.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 远程仓库/远程仓库/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  远程仓库
+//
+//  Created by lanou on 15/11/9.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
